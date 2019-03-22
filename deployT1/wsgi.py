ce06306560e54dc5c8d334d2ab9f7c1761df30a9
mealvarado <mealvarado@uc.cl>
2019-03-22 17:35:21 -0300

    deploy

---
+++ deployT1/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for deployT1 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deployT1.settings')
+
+application = get_wsgi_application()
+
+# Use whitenoise package to serve static files on Heroku
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
